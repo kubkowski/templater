5678eca4af15812482bd5f24a46457cca28dc136
kubkowski <jakub.opara@gmail.com>
2015-01-31 18:38:02 +0100

    Added CMS Controller test + implementation

---
+++ app/controllers/cms_controller.rb
@@ -0,0 +1,12 @@
+class CmsController < ActionController::Metal
+  include ActionController::Rendering
+  include AbstractController::Helpers
+
+  prepend_view_path SqlTemplate::Resolver.instance
+  helper CmsHelper
+
+  def respond
+    render template: params[:page]
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,18 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "capybara"
+require "capybara/rails"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+  include Capybara::DSL
+  include Rails.application.routes.url_helpers
 end
